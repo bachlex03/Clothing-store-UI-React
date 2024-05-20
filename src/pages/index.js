78af0eb4ebd25dec4b3747e97bc830ae0ee77ff5
tqphuong3011 <122359573+tqphuong3011@users.noreply.github.com>
2024-05-21 00:17:33 +0700

    fix request pull 21/05/2024

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 export { default as Home } from './Home';
 export { default as Shop } from './Shop';
 export { default as Location } from './Location';
-export { default as Contact } from './Contact';export { default as Cart } from './Cart';
+export { default as Contact } from './Contact';
+export { default as Cart } from './Cart';
 export { default as Details } from './Details';
 export { default as Checkout } from './Checkout';
 export { default as Login } from './Login';
